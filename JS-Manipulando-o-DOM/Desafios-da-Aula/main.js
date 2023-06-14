37277b75609be4a11354dad6de1b9bde8dde1699
Arthur Korkiskis Felismino <arthurkfelismino@gmail.com>
2023-06-14 17:48:25 -0300

    adicionando estilos e animação JS

--- JS-Manipulando-o-DOM/Desafios-da-Aula/main.js
+++ JS-Manipulando-o-DOM/Desafios-da-Aula/main.js
@@ -1,1 +1,15 @@
 document.querySelector('#texto').innerHTML="O INVERNO CHEGOU"
+
+document.querySelector('#texto').animate(
+    [
+        {
+            opacity: 0,
+            color: "#fff",
+          },
+          {
+            opacity: 1,
+            color: "#000",
+          },
+    ],
+    2000
+);
